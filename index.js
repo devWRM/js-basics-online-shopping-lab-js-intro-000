0a885706225b12a012670ab2e9d8ef0211350c48
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-19 13:31:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,56 @@ function setCart(c) {
   return cart;
 }
 
+
+
 function addToCart(item) {
  // write your code here
+
+var randomPrice = (Math.floor(Math.random() * 100) + 1);
+cart.push({itemName: item, itemPrice: randomPrice});
+return `${item} has been added to your cart.`;
+
 }
+
+
+
 
 function viewCart() {
   // write your code here
+    if (cart.length == 0) {
+      return `Your shopping cart is empty.`;
+    }
+
+    var cartIntro = `In your cart, you have`;
+    var itemsInCart = [];
+
+    for (var i = 0; i < cart.length; i++) {
+      if (cart.length === 1) {
+          itemsInCart.push(` ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+      } else if (i === cart.length - 1) {
+          itemsInCart.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+      } else {
+          itemsInCart.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+      }
+    }
+
+    return cartIntro + itemsInCart;
 }
+
+
 
 function total() {
   // write your code here
+  if (cart.length == 0) {
+		return `Your shopping cart is empty.`;
+	}
+
+	var cartTotal = 0;
+	for (var i = 0; i < cart.length; i++) {
+		cartTotal = cartTotal + cart[i].itemPrice;
+	}
+
+	return cartTotal;
 }
 
 function removeFromCart(item) {
